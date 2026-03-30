5b5fb06affe38decc0d6f3e1cb25464a7403e460
venkai1234 <venkateswarulu@kluniversity.in>
2026-03-30 10:12:38 +0530

    backend commit

---
+++ src/main/java/com/klu/S5SpringbootMvcCrudDbApplication.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5SpringbootMvcCrudDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5SpringbootMvcCrudDbApplication.class, args);
+	}
+
+}
